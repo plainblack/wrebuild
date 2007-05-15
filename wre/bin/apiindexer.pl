1a537a1b68063a050459c4a496ed5309299c628d
JT Smith <jt@plainblack.com>
2007-05-15 02:11:13 +0000

    added an api documentation server renamed the webgui search indexer plugins

---
+++ wre/bin/apiindexer.pl
@@ -0,0 +1,10 @@
+#!/data/wre/prereqs/bin/perl
+use lib '/data/WebGUI/lib';
+use strict;
+use Pod::POM::Web::Indexer;
+print "Indexing POD content...\n";
+Pod::POM::Web::Indexer->new->index(-from_scratch => 1);
+print "\nIndexing complete.\n";
+
+
+
---
+++ wre/bin/apiwebserver.pl
@@ -0,0 +1,10 @@
+#!/data/wre/prereqs/bin/perl
+use lib '/data/WebGUI/lib';
+use strict;
+use Pod::POM::Web;
+Pod::POM::Web->server(8095);
+print "Point your web browser to http://localhost:8095/ to view the API documentation. 
+
+
+
+
