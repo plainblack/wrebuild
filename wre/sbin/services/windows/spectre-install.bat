ea722c27904dc42d7c804d7cded1e33bee763628
JT Smith <jt@plainblack.com>
2007-10-02 15:48:34 +0000

    fix spectre install

--- wre/sbin/services/windows/spectre-install.bat
+++ wre/sbin/services/windows/spectre-install.bat
@@ -1,4 +1,5 @@
 c:\data\wre\prereqs\bin\instsrv.exe WREspectre c:\data\wre\prereqs\bin\srvany.exe
-reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters\Application c:\data\wre\prereqs\bin\perl.exe
-reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters\AppParameters c:\data\WebGUI\sbin\spectre.pl --run
-reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters\AppDirectory c:\data\WebGUI\sbin
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters /v Application /t REG_SZ /d c:\data\wre\prereqs\bin\perl.exe
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters /v AppParameters /t REG_SZ /d "c:\data\WebGUI\sbin\spectre.pl --run"
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters /v AppDirectory /t REG_SZ /d c:\data\WebGUI\sbin
