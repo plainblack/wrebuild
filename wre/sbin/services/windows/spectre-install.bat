fc4ea23e9f0da1702442889c06c8becd88ec4805
Graham Knop <graham@plainblack.com>
2007-10-02 15:27:51 +0000

    install spectre using srvany

--- wre/sbin/services/windows/spectre-install.bat
+++ wre/sbin/services/windows/spectre-install.bat
@@ -1,1 +1,4 @@
-c:\data\wre\prereqs\bin\instsrv.exe WREspectre c:\data\wre\sbin\services\windows\spectre-service.bat
+c:\data\wre\prereqs\bin\instsrv.exe WREspectre c:\data\wre\prereqs\bin\srvany.exe
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters\Application c:\data\wre\prereqs\bin\perl.exe
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters\AppParameters c:\data\WebGUI\sbin\spectre.pl --run
+reg add HKLM\SYSTEM\CurrentControlSet\Services\WREspectre\Parameters\AppDirectory c:\data\WebGUI\sbin
