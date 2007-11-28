7affee0df27977eb67420147cd3ba6d493ff986a
Graham Knop <graham@plainblack.com>
2007-11-28 22:26:57 +0000

    Stats and path fix for windows

--- wre/sbin/services/windows/modperl-install.bat
+++ wre/sbin/services/windows/modperl-install.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enableextensions
-for /f "usebackq tokens=1,3" %%d in (`reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH`) do (
-  if "%%d"=="PATH" set globalpath=";%%e;"
+for /f "usebackq tokens=1,2*" %%d in (`reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH`) do (
+  if "%%d"=="PATH" set globalpath=";%%f;"
 )
 
 set globalpath=%globalpath:;C:\data\wre\bin;=;%
@@ -13,6 +13,5 @@ set globalpath=%globalpath:;C:\data\wre\prereqs\modules\;=;%
 
 set globalpath="%globalpath:~2,-2%;C:\data\wre\bin;C:\data\wre\prereqs\bin;C:\data\wre\prereqs\modules"
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "PATH" /t REG_SZ /d %globalpath% /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "PATH" /t REG_EXPAND_SZ /d %globalpath% /f
 c:\data\wre\prereqs\bin\Apache.exe -k install -n WREmodperl -f c:\data\wre\etc\modperl.conf
-
