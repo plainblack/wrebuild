afee55941891732852ac7b0e94e89e167f989290
JT Smith <jt@plainblack.com>
2008-02-21 16:47:00 +0000

    Changed modproxy configs to pass file privilege checks through to WebGUI.

--- wre/var/upgrades/upgrade_0.8.1-0.8.2.pl
+++ wre/var/upgrades/upgrade_0.8.1-0.8.2.pl
@@ -3,6 +3,7 @@
 use strict;
 use lib '/data/wre/lib';
 use WRE::Config;
+use WRE::File;
 
 my $config = WRE::Config->new;
 
@@ -13,11 +14,26 @@ print "\tUpdating version number to $version.";
 $config->set("version",$version);
 print "\tOK\n";
 
-#example($config);
+enablingFilePrivilegeChecking($config);
 
-#sub example {
-#    my $config = shift;
-#    print "\tDoing something...";
-#    # .. do work here ..
-#    print "\tOK\n";
-#}
+sub enablingFilePrivilegeChecking {
+    my $config = shift;
+    print "\tEnabling file privilege checking...";
+    my $file = WRE::File->new($config);
+    my $dir = opendir("/data/wre/etc");
+    foreach my $file (readdir($dir)) { 
+        next unless ($file =~ m/.modproxy$/);
+        my $contents = $file->slurp("/data/wre/etc/".$file);
+        ${$contents} =~ s{
+          RewriteRule ^/uploads/ - [L] 
+        }{
+            # For speed we only pass on uploads if there is a .wgaccess file
+            RewriteCond %{REQUEST_FILENAME}             ^(.*/)     # Match up to the last /  - save the directory in %1
+            RewriteCond ${DOCUMENT_ROOT}%1.wgaccess     !-f        # if (root + directory + .wgaccess) doesn't exist,
+            RewriteRule ^/uploads/                      - [L]      # serve directly
+        }xms;
+        $file->spit("/data/wre/etc/".$file, $contents);
+    }
+    closedir($dir);
+    print "\tOK\n";
+}
