45360f0be699d2760a2e98ae749c23dae312e28b
Koen de Jonge <koen@procolix.com>
2009-03-03 17:46:29 +0000

    Tools for developers in a separate directory.

---
+++ developers/compilemodules.sh
@@ -0,0 +1,60 @@
+cd Carp-Assert-0.20
+perl Makefile.pl
+make
+make install
+cd ../Test-Exception-0.27
+perl Makefile.pl
+make
+make install
+cd ../Carp-Assert-More-1.12
+perl Makefile.pl
+make
+make install
+cd ../HTTP-Server-Simple-0.38
+perl Makefile.pl
+make
+make install
+cd ../Test-LongString-0.11
+perl Makefile.pl
+make
+make install
+cd ../HTTP-Response-Encoding-0.05
+perl Makefile.pl
+make
+make install
+cd ../Array-Compare-1.17
+perl Makefile.pl
+make
+make install
+cd ../Tree-DAG_Node-1.06
+perl Makefile.pl
+make
+make install
+cd ../Test-Warn-0.11
+perl Makefile.pl
+make
+make install
+cd ../Devel-Cycle-1.10
+perl Makefile.pl
+make
+make install
+cd ../PadWalker-1.7
+perl Makefile.pl
+make
+make install
+cd ../Test-Memory-Cycle-1.04
+perl Makefile.pl
+make
+make install
+cd ../Test-Taint-1.04
+perl Makefile.pl
+make
+make install
+cd ../WWW-Mechanize-1.54
+perl Makefile.pl
+make
+make install
+cd ../Test-WWW-Mechanize-1.24
+perl Makefile.pl
+make
+make install
---
+++ developers/readme.txt
@@ -0,0 +1,3 @@
+This download script is for convienience, it contains all perl modules to add to the WRE to run the testsuite.
+
+These scripts are not recommended for production WRE installations.
