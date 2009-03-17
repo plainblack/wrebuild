bb77a6b73b9b885aa455e18bb0bec5f4993872da
JT Smith <jt@plainblack.com>
2009-03-17 22:43:56 +0000

    changing upgrade to upgrade mysql tables and config file to 5.1

--- wre/var/upgrades/upgrade_0.8.5-0.9.0.pl
+++ wre/var/upgrades/upgrade_0.8.5-0.9.0.pl
@@ -3,8 +3,23 @@
 use strict;
 use lib '/data/wre/lib';
 use WRE::Config;
+use WRE::Mysql;
 
 my $config = WRE::Config->new;
+
+
+# updating mysql config
+print "\tUpdating MySQL config file.";
+system("/data/wre/prereqs/bin/perl -i -p -e's[skip-innodb][]g' /data/wre/etc/my.cnf");
+print "\tOK\n";
+
+# upgrading mysql
+print "\tUpgrading MySQL tables to 5.1...\n";
+my $mysql = WRE::Mysql->new({wreConfig=>$config});
+$mysql->start;
+print "Enter your MySQL ".$config->get("mysql")->{adminUser}." password.\n";
+system("/data/wre/prereqs/bin/mysql_upgrade -u".$config->get("mysql")->{adminUser}." -p");
+print "\tOK\n";
 
 
 # changing version number
@@ -12,6 +27,8 @@ my $version = "0.9.0";
 print "\tUpdating version number to $version.";
 $config->set("version",$version);
 print "\tOK\n";
+
+
 
 #example($config);
 
