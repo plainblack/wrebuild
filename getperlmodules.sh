226a9fcdeb98c0c2d0b0d876ec68309d5fed16d6
JT Smith <jt@plainblack.com>
2009-04-15 18:25:48 +0000

    screwed up urls, now fixed

--- getperlmodules.sh
+++ getperlmodules.sh
@@ -548,51 +548,51 @@ wget -t 4 -nv $CPANMIRROR/authors/id/D/DA/DAGOLDEN/Sub-Uplevel-0.2002.tar.gz
 tar zxf Sub-Uplevel-0.2002.tar.gz
 
 # http://search.cpan.org/dist/Carp-Assert/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/M/MS/MSCHWERN/Carp-Assert-0.20.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/M/MS/MSCHWERN/Carp-Assert-0.20.tar.gz
 tar zxf Carp-Assert-0.20.tar.gz
 
 # http://search.cpan.org/dist/Test-Exception/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/A/AD/ADIE/Test-Exception-0.27.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/A/AD/ADIE/Test-Exception-0.27.tar.gz
 tar zxf Test-Exception-0.27.tar.gz
 
 # http://search.cpan.org/dist/Carp-Assert-More/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/P/PE/PETDANCE/Carp-Assert-More-1.12.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/P/PE/PETDANCE/Carp-Assert-More-1.12.tar.gz
 tar zxf Carp-Assert-More-1.12.tar.gz
 
 # http://search.cpan.org/dist/HTTP-Server-Simple/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/J/JE/JESSE/HTTP-Server-Simple-0.38.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/J/JE/JESSE/HTTP-Server-Simple-0.38.tar.gz
 tar zxf HTTP-Server-Simple-0.38.tar.gz
 
 # http://search.cpan.org/dist/Test-LongString/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/R/RG/RGARCIA/Test-LongString-0.11.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/R/RG/RGARCIA/Test-LongString-0.11.tar.gz
 tar zxf Test-LongString-0.11.tar.gz
 
 # http://search.cpan.org/dist/HTTP-Response-Encoding/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/D/DA/DANKOGAI/HTTP-Response-Encoding-0.05.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/D/DA/DANKOGAI/HTTP-Response-Encoding-0.05.tar.gz
 tar zxf HTTP-Response-Encoding-0.05.tar.gz
 
 # http://search.cpan.org/dist/Array-Compare/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/D/DA/DAVECROSS/Array-Compare-1.17.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/D/DA/DAVECROSS/Array-Compare-1.17.tar.gz
 tar zxf Array-Compare-1.17.tar.gz
 
 # http://search.cpan.org/dist/Tree-DAG_Node/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/C/CO/COGENT/Tree-DAG_Node-1.06.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/C/CO/COGENT/Tree-DAG_Node-1.06.tar.gz
 tar zxf Tree-DAG_Node-1.06.tar.gz
 
 # http://search.cpan.org/dist/Test-Warn/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/C/CH/CHORNY/Test-Warn-0.11.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/C/CH/CHORNY/Test-Warn-0.11.tar.gz
 tar zxf Test-Warn-0.11.tar.gz
 
 # http://search.cpan.org/dist/Devel-Cycle/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/L/LD/LDS/Devel-Cycle-1.10.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/L/LD/LDS/Devel-Cycle-1.10.tar.gz
 tar zxf Devel-Cycle-1.10.tar.gz
 
 # http://search.cpan.org/dist/PadWalker/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/R/RO/ROBIN/PadWalker-1.7.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/R/RO/ROBIN/PadWalker-1.7.tar.gz
 tar zxf PadWalker-1.7.tar.gz
 
 # http://search.cpan.org/dist/Test-Memory-Cycle/
-wget -t 4 -nv $CPANMIRROR/CPAN/authors/id/P/PE/PETDANCE/Test-Memory-Cycle-1.04.tar.gz
+wget -t 4 -nv $CPANMIRROR/authors/id/P/PE/PETDANCE/Test-Memory-Cycle-1.04.tar.gz
 tar zxf Test-Memory-Cycle-1.04.tar.gz
 
 # http://search.cpan.org/dist/Test-Taint/
