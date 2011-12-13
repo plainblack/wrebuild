5154b1e416eb14ac5c042b549326bdd74a27c1b9
JT Smith <jt@plainblack.com>
2011-12-12 19:23:44 -0600

    fixed getting sources

--- getsource.sh
+++ getsource.sh
@@ -7,17 +7,32 @@
 mkdir source
 cd source
 
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.10.tar.gz
+tar xfz pcre-8.10.tar.gz
+
+wget http://openssl.org/source/openssl-1.0.0c.tar.gz
+tar xvz openssl-1.0.0c.tar.gz
+
+wget http://nginx.org/download/nginx-0.7.67.tar.gz
+tar xfz nginx-0.7.67.tar.gz
+
+wget http://www.monkey.org/~provos/libevent-1.4.14b-stable.tar.gz
+tar xfz libevent-1.4.14b-stable.tar.gz
+
+wget http://memcached.googlecode.com/files/memcached-1.4.5.tar.gz
+tar xfz memcached-1.4.5.tar.gz
+
 # ncurses http://www.gnu.org/software/ncurses/
-wget -t 4 -nv http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.7.tar.gz
-tar xfz ncurses-5.7.tar.gz
+#wget -t 4 -nv http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.7.tar.gz
+#tar xfz ncurses-5.7.tar.gz
 
 # readline http://tiswww.case.edu/php/chet/readline/rltop.html
-wget -t 4 -nv ftp://ftp.cwru.edu/pub/bash/readline-6.1.tar.gz
-tar xfz readline-6.1.tar.gz
+#wget -t 4 -nv ftp://ftp.cwru.edu/pub/bash/readline-6.1.tar.gz
+#tar xfz readline-6.1.tar.gz
 
 # tiff for image magick http://www.libtiff.org/
-wget -t 4 -nv ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.8.2.tar.gz
-tar xfz tiff-3.8.2.tar.gz
+#wget -t 4 -nv ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.8.2.tar.gz
+#tar xfz tiff-3.8.2.tar.gz
 
 # imagemagick http://www.imagemagick.org/script/index.php
 wget -t 4 -nv ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick.tar.gz
@@ -35,9 +50,9 @@ wget -t 4 -nv ftp://ftp.gnu.org/gnu/aspell/dict/en/aspell6-en-6.0-0.tar.bz2
 tar jxf aspell6-en-6.0-0.tar.bz2
 
 # awstats http://awstats.sourceforge.net/
-wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/awstats/awstats-6.95.tar.gz
-tar zxf awstats-6.95.tar.gz
-cd awstats-6.95/wwwroot
+wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/awstats/awstats-7.0.tar.gz
+tar zxf awstats-7.0.tar.gz
+cd awstats-7.0/wwwroot
 mv cgi-bin/* ./
 perl -ni -e 'print unless /^\s*if . !\$FileConfig/ .. /^\s+}/; print $_.qq/\t\terror("Could not open config file");\n\t}\n/ if /^\s*if . !\$FileConfig/;' awstats.pl
 perl -0777 -pi -e 's!else\s*{\s+\@PossibleConfigDir\s+=\s+.+?\);!else {\n\t\t\@PossibleConfigDir = ("\$DIR", "/data/wre/etc");!ms;' awstats.pl
@@ -48,81 +63,81 @@ wget -t 4 -nv http://ftp.wagner.pp.ru/pub/catdoc/catdoc-0.94.2.tar.gz
 tar zxf catdoc-0.94.2.tar.gz
 
 # expat xml parser http://expat.sourceforge.net/
-wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/expat/expat-2.0.1.tar.gz
-tar zxf expat-2.0.1.tar.gz
+#wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/expat/expat-2.0.1.tar.gz
+#tar zxf expat-2.0.1.tar.gz
 
 # freetype portable font engine http://freetype.sourceforge.net/index2.html
 wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/freetype/freetype-2.3.11.tar.bz2
 tar jxf freetype-2.3.11.tar.bz2
 
 # gnutls transport layer security http://www.gnu.org/software/gnutls/
-wget -t 4 -nv ftp://ftp.gnu.org/pub/gnu/gnutls/gnutls-2.8.5.tar.bz2
-tar jxf gnutls-2.8.5.tar.bz2
+#wget -t 4 -nv ftp://ftp.gnu.org/pub/gnu/gnutls/gnutls-2.8.5.tar.bz2
+#tar jxf gnutls-2.8.5.tar.bz2
 
 # httpd apache webserver http://httpd.apache.org/
-wget -t 4 -nv http://www.apache.org/dist/httpd/httpd-2.2.20.tar.gz
-tar zxf httpd-2.2.20.tar.gz
+#wget -t 4 -nv http://www.apache.org/dist/httpd/httpd-2.2.20.tar.gz
+#tar zxf httpd-2.2.20.tar.gz
 
 # lftp sophisticated ftp client http://lftp.yar.ru/
 wget -t 4 -nv http://ftp.yars.free.net/pub/source/lftp/old/lftp-4.2.0.tar.gz
 tar zxf lftp-4.2.0.tar.gz
 
 # libiconv unicode conversion tool http://www.gnu.org/software/libiconv/
-wget -t 4 -nv http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.13.tar.gz
-tar zxf libiconv-1.13.tar.gz
+#wget -t 4 -nv http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.13.tar.gz
+#tar zxf libiconv-1.13.tar.gz
 
 # libjpeg image manipulation http://www.ijg.org/
-wget -t 4 -nv http://www.ijg.org/files/jpegsrc.v7.tar.gz
-tar zxf jpegsrc.v7.tar.gz
-mv jpeg-7 libjpeg-7
+wget http://www.ijg.org/files/jpegsrc.v8c.tar.gz
+tar xfz jpegsrc.v8c.tar.gz
 
 # lcms color management http://www.littlecms.com/
-wget -t 4 -nv http://sourceforge.net/projects/lcms/files/lcms/1.19/lcms-1.19.tar.gz/download
-tar zxf lcms-1.19.tar.gz
-.3
+#wget -t 4 -nv http://sourceforge.net/projects/lcms/files/lcms/1.19/lcms-1.19.tar.gz/download
+#tar zxf lcms-1.19.tar.gz
+#.3
 # graphviz graph generator http://www.graphviz.org/
 wget -t 4 -nv http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.24.0.tar.gz
 tar xfz graphviz-2.24.0.tar.gz
 
 # libpng image manipulation http://www.libpng.org/pub/png/libpng.html
-wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/libpng/libpng-1.2.35.tar.bz2
-tar jxf libpng-1.2.35.tar.bz2
+wget ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5.4.tar.gz
+tar xfz libpng-1.5.4.tar.gz
 
 # gnu libtool http://www.gnu.org/software/libtool/
-wget -t 4 -nv http://ftp.gnu.org/gnu/libtool/libtool-2.2.6a.tar.gz
-tar zxf libtool-2.2.6a.tar.gz
+#wget -t 4 -nv http://ftp.gnu.org/gnu/libtool/libtool-2.2.6a.tar.gz
+#tar zxf libtool-2.2.6a.tar.gz
 
 # libgif image manipulation http://sourceforge.net/projects/giflib/
 wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/giflib/giflib-4.1.6.tar.bz2
 tar jxf giflib-4.1.6.tar.bz2
 
 # libgcrypt crypt library http://www.gnupg.org/download/
-wget -t 4 -nv ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.4.6.tar.bz2
-tar jxf libgcrypt-1.4.6.tar.bz2
+#wget -t 4 -nv ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.4.6.tar.bz2
+#tar jxf libgcrypt-1.4.6.tar.bz2
 
 # libgpg-error error messages http://www.gnupg.org/download/
-wget -t 4 -nv ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.7.tar.bz2
-tar jxf libgpg-error-1.7.tar.bz2
+#wget -t 4 -nv ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.7.tar.bz2
+#tar jxf libgpg-error-1.7.tar.bz2
 
 # libxml2 xml c parser http://www.xmlsoft.org/
-wget -t 4 -nv ftp://xmlsoft.org/libxml2/libxml2-2.7.6.tar.gz
-tar zxf libxml2-2.7.6.tar.gz
+wget ftp://anonymous@xmlsoft.org/libxml2/libxml2-sources-2.7.7.tar.gz
+tar xfz libxml2-sources-2.7.7.tar.gz
+
 
 # modperl apache perl module http://perl.apache.org
-wget -t 4 -nv http://perl.apache.org/dist/mod_perl-2.0-current.tar.gz
-tar zxf mod_perl-2.0-current.tar.gz
+#wget -t 4 -nv http://perl.apache.org/dist/mod_perl-2.0-current.tar.gz
+#tar zxf mod_perl-2.0-current.tar.gz
 
 # mysql database server http://dev.mysql.com/downloads/mysql/5.0.html
-wget -t 4 -nv http://downloads.mysql.com/archives/mysql-5.0/mysql-5.0.89.tar.gz
-tar zxf mysql-5.0.89.tar.gz
+#wget -t 4 -nv http://downloads.mysql.com/archives/mysql-5.0/mysql-5.0.89.tar.gz
+#tar zxf mysql-5.0.89.tar.gz
 
 # openssl ssl toolkit http://www.openssl.org/
-wget -t 4 -nv http://www.openssl.org/source/openssl-0.9.8o.tar.gz
-tar zxf openssl-0.9.8o.tar.gz
+#wget -t 4 -nv http://www.openssl.org/source/openssl-0.9.8o.tar.gz
+#tar zxf openssl-0.9.8o.tar.gz
 
 # perl programming language http://www.cpan.org/src/README.html
-wget -t 4 -nv http://www.cpan.org/src/perl-5.10.1.tar.gz
-tar zxf perl-5.10.1.tar.gz
+wget -t 4 -nv http://www.cpan.org/src/perl-5.14.2.tar.gz
+tar zxf perl-5.14.2.tar.gz
 
 # perlmodules
 # SEE SEPARATE SCRIPT
@@ -140,22 +155,22 @@ wget -t 4 -nv ftp://ftp.foolabs.com/pub/xpdf/xpdf-3.02pl4.patch
 patch -p0 <xpdf-3.02pl4.patch
 
 # zlib compression library http://www.zlib.net/
-wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/libpng/zlib-1.2.3.tar.bz2
-tar jxf zlib-1.2.3.tar.bz2
+#wget -t 4 -nv http://surfnet.dl.sourceforge.net/sourceforge/libpng/zlib-1.2.3.tar.bz2
+#tar jxf zlib-1.2.3.tar.bz2
 
 # rsync http://www.samba.org/rsync/download.html
 wget -t 4 -nv http://rsync.samba.org/ftp/rsync/src/rsync-3.0.6.tar.gz
 tar xfz rsync-3.0.6.tar.gz
 
 # git http://git-scm.com/download
-wget -t 4 -nv http://kernel.org/pub/software/scm/git/git-1.7.6.tar.bz2
-tar jxf git-1.7.6.tar.bz2
+#wget -t 4 -nv http://kernel.org/pub/software/scm/git/git-1.7.6.tar.bz2
+#tar jxf git-1.7.6.tar.bz2
 
 # http://curl.haxx.se/
-wget -t 4 -nv http://curl.haxx.se/download/curl-7.19.7.tar.bz2
-tar jxf curl-7.19.7.tar.bz2
+#wget -t 4 -nv http://curl.haxx.se/download/curl-7.19.7.tar.bz2
+#tar jxf curl-7.19.7.tar.bz2
 
-rm -f *.gz *.tgz *.bz2 *.zip
+#rm -f *.gz *.tgz *.bz2 *.zip
 
 
 
