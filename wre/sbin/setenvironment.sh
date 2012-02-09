6e80518dad57d6f3420bc1c26af0148c7cd1f3dd
Colin Kuskie <colink@perldreamer.com>
2012-02-09 15:52:13 -0800

    Need to know how to find WRE libraries for the wre.psgi

--- wre/sbin/setenvironment.sh
+++ wre/sbin/setenvironment.sh
@@ -2,4 +2,4 @@ export PATH=/data/wre/prereqs/bin:/data/wre/prereqs/sbin:/data/wre/sbin:/data/wr
 export LD_LIBRARY_PATH=/data/wre/prereqs/lib:$LD_LIBRARY_PATH
 export DYLD_FALLBACK_LIBRARY_PATH=/data/wre/prereqs/lib:$DYLD_LIBRARY_PATH
 export LD_RUN_PATH=/data/wre/prereqs/lib:$LD_RUN_PATH
-export PERL5LIB=/data/WebGUI/lib:$PERL5LIB
+export PERL5LIB=/data/wre/lib:/data/WebGUI/lib:$PERL5LIB
