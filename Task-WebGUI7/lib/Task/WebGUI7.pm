0bc2aeb6edbb3a4c7def9aef72daf48d4cb82a51
David Delikat <david@plainblack.com>
2015-07-23 12:02:11 -0500

    task::webgui7

---
+++ Task-WebGUI7/lib/Task/WebGUI7.pm
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+package Task::WebGUI7;
+
+1;
+
+=head1 NAME
+
+Task::WebGUI7 - Get all the prereqs for WebGUI
+
+=head1 SYNOPSIS
+
+ cpanm Task::WebGUI7
+
+=head1 DESCRIPTION
+
+Installing Task::WebGUI7 will ensure that you have all the prereq modules needed to run WebGUI (L<http://www.webgui.org>).
+
+=head1 AUTHOR
+
+JT Smith (jt at plainblack dot com)
+
