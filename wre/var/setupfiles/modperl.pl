b0c7ae57ab73b357b53944248272b83b23a0feee
colin <colink@perldreamer.com>
2016-02-17 15:04:56 -0800

    Update settings for 64-bit

--- wre/var/setupfiles/modperl.pl
+++ wre/var/setupfiles/modperl.pl
@@ -1,6 +1,6 @@
 use Apache2::SizeLimit;
-$Apache2::SizeLimit::MAX_PROCESS_SIZE  = 250000;
-$Apache2::SizeLimit::MAX_UNSHARED_SIZE = 175000;
+$Apache2::SizeLimit::MAX_PROCESS_SIZE  = 750000;
+$Apache2::SizeLimit::MAX_UNSHARED_SIZE = 750000;
 $Apache2::SizeLimit::CHECK_EVERY_N_REQUESTS = 5;
 
 1;
